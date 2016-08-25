61cf3488253e6710e8d6ed81f8c9ae3a0bd68f2b
Trev <trevor.pawlewicz@gmail.com>
2016-08-25 17:39:25 -0400

    corrected index.ejs

--- app.js
+++ app.js
@@ -47,7 +47,15 @@ app.get('/blogs/new', function(req, res){
     res.render('new');
 });
 // CREATE route
-
+app.post('/blogs', function(req, res){
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
+            res.render('new');
+        } else {
+            res.redirect('/blogs');
+        }
+    })
+});
 
 
 
